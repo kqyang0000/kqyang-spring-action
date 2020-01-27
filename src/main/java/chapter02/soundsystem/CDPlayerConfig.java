1803e07e9aa66d473886d49636e66a2699791eb6
kqyang <kqyang0000@163.com>
2020-01-27 13:46:29 +0800

    通过java代码显示装配Bean

--- src/main/java/chapter02/soundsystem/CDPlayerConfig.java
+++ src/main/java/chapter02/soundsystem/CDPlayerConfig.java
@@ -1,9 +1,23 @@
 package chapter02.soundsystem;
 
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan(basePackages = {"chapter02.soundsystem"})
 public class CDPlayerConfig {
+
+    /**
+     * 默认情况下，bean的ID与带有@Bean注解的方法名是一样的
+     *
+     * @return
+     */
+    @Bean(name = "lonelyHeartsClubBand")
+    public CompactDisc sgtPeppers() {
+        return new SgtPeppers();
+    }
+
+    @Bean
+    public MediaPlayer mediaPlayer(CompactDisc cd) {
+        return new CDPlayer(cd);
+    }
 }
